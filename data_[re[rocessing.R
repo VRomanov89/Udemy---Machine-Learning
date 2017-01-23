2d096b1ea056e289f87a104d07289fabd1ffeb9a
Vladimir Romanov <vladromweb@gmail.com>
2017-01-22 17:20:41 -0800

    Fixing data
    Fixing data

---
+++ data_[re[rocessing.R
@@ -0,0 +1,1 @@
+dataset = read.csv('Data.csv')
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,3 +6,14 @@ Created on Sun Jan 22 16:24:50 2017
 @author: VladRomanov
 """
 
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+#Fixing missing data
